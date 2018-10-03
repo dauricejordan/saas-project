6a7079368aabb5a97e13d20b6ac80e2ab71b6a18
Finance Tracker <dauricejordan@gmail.com>
2018-10-02 22:38:26 -0400

    Add stripe and sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = {
+    :host => 'daurice-jordan-saas.herokuapp.com/',
+    :protocol => 'https'
+  }
 end
